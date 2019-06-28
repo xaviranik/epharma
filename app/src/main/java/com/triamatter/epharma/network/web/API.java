5c49671cd1c07d5adaba1971cbfa7d886bfaf659
Xavir <xaviranik.me@gmail.com>
2019-06-28 22:32:18 +0600

    Added: ProductRequest on Network package, Dynamic product loading based on category, Dynamic latest product

--- app/src/main/java/com/triamatter/epharma/network/web/API.java
+++ app/src/main/java/com/triamatter/epharma/network/web/API.java
@@ -3,4 +3,5 @@ package com.triamatter.epharma.network.web;
 public class API {
     public static final String GET_CATEGORY = "http://192.168.0.102/epharma/category.php";
     public static final String GET_SUBCATEGORY = "http://192.168.0.102/epharma/subcategory.php";
+    public static final String GET_PRODUCT = "http://192.168.0.102/epharma/product.php";
 }
