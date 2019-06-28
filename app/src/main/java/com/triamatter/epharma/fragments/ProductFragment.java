3ecf517202514957ec48d7b54b162876cf1ab9cf
Xavir <xaviranik.me@gmail.com>
2019-06-28 10:47:21 +0600

    Added: ProductFragment add/remove product button funtionality

--- app/src/main/java/com/triamatter/epharma/fragments/ProductFragment.java
+++ app/src/main/java/com/triamatter/epharma/fragments/ProductFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,13 +16,17 @@ import com.triamatter.epharma.activities.MainActivity;
 import com.triamatter.epharma.network.KEYS;
 import com.triamatter.epharma.utils.Utils;
 
-public class ProductFragment extends Fragment {
+public class ProductFragment extends Fragment implements View.OnClickListener{
 
     private String productName;
     private float productPrice;
 
     private TextView textViewProductName;
     private TextView textViewProductPrice;
+
+    private ImageView buttonAddQuantity;
+    private ImageView buttonRemoveQuantity;
+    private TextView textViewQuantity;
 
     @Nullable
     @Override
@@ -38,6 +43,13 @@ public class ProductFragment extends Fragment {
         textViewProductName = (TextView) view.findViewById(R.id.textView_product_name);
         textViewProductPrice = (TextView) view.findViewById(R.id.textView_product_price);
 
+        buttonAddQuantity = (ImageView) view.findViewById(R.id.button_add_quantity);
+        buttonRemoveQuantity = (ImageView) view.findViewById(R.id.button_minus_quantity);
+        textViewQuantity = (TextView) view.findViewById(R.id.textView_product_quantity);
+
+        buttonAddQuantity.setOnClickListener((View.OnClickListener) this);
+        buttonRemoveQuantity.setOnClickListener((View.OnClickListener) this);
+
         getArgumentsFromPreviousFragment();
     }
 
@@ -53,4 +65,32 @@ public class ProductFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.button_add_quantity:
+            {
+                manageQuantity(true);
+                break;
+            }
+            case R.id.button_minus_quantity:
+            {
+                manageQuantity(false);
+                break;
+            }
+        }
+    }
+
+    private void manageQuantity(boolean addButton)
+    {
+        int quantity = Integer.valueOf(textViewQuantity.getText().toString());
+        quantity = addButton ? quantity + 1 : quantity - 1;
+        if(quantity <= 0)
+        {
+            quantity = 0;
+        }
+        textViewQuantity.setText(String.valueOf(quantity));
+    }
 }
