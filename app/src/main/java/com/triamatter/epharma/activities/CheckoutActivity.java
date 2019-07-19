3e2b9b53684b8465a1d48bbc24c6537024a0b76b
Xavir <xaviranik.me@gmail.com>
2019-07-19 16:12:46 +0600

    Added: Delivery Zone spinner, Tweaked CheckoutActivity UI

--- app/src/main/java/com/triamatter/epharma/activities/CheckoutActivity.java
+++ app/src/main/java/com/triamatter/epharma/activities/CheckoutActivity.java
@@ -3,19 +3,20 @@ package com.triamatter.epharma.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.carteasy.v1.lib.Carteasy;
+import com.jaredrummler.materialspinner.MaterialSpinner;
 import com.triamatter.epharma.R;
 import com.triamatter.epharma.model.Product;
 import com.triamatter.epharma.network.NetworkSingleton;
@@ -28,7 +29,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -44,10 +44,11 @@ public class CheckoutActivity extends AppCompatActivity {
 
     private int totalQuantity = 0;
     private float subTotalPrice = 0;
-    private float deliveryCharge = 0;
+    private float deliveryCharge = 60;
     private int discount = 0;
     private float totalPrice = 0;
     private long orderID;
+    private String couponCode;
 
     int productID;
     String productName;
@@ -60,7 +61,12 @@ public class CheckoutActivity extends AppCompatActivity {
     private TextView textViewTotal;
     private TextView textViewDiscount;
 
+    private EditText editTextCoupon;
+
     private Button checkoutButton;
+    private Button addCouponButton;
+
+    private MaterialSpinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -73,8 +79,13 @@ public class CheckoutActivity extends AppCompatActivity {
         textViewDiscount = (TextView) findViewById(R.id.textView_discount_percent);
         textViewTotal = (TextView) findViewById(R.id.textView_total_price);
         textViewDeliveryCharge = (TextView) findViewById(R.id.textView_delivery_charge);
+        editTextCoupon = (EditText) findViewById(R.id.editText_coupon);
+        spinner = (MaterialSpinner) findViewById(R.id.spinner);
 
         checkoutButton = (Button) findViewById(R.id.button_place_order);
+        addCouponButton = (Button) findViewById(R.id.button_add_coupon);
+        addCouponButton.setVisibility(View.GONE);
+
         checkoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
@@ -84,7 +95,56 @@ public class CheckoutActivity extends AppCompatActivity {
         });
 
 
+        getDeliveryZone();
         getProductList();
+
+        editTextCoupon.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+                if(charSequence.length() == 0)
+                {
+                    addCouponButton.setVisibility(View.GONE);
+                }
+                else
+                {
+                    addCouponButton.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+
+            }
+        });
+    }
+
+    private void getDeliveryZone()
+    {
+        spinner.setItems("Inside Dhaka", "Outside Dhaka");
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item)
+            {
+                if(position == 0)
+                {
+                    deliveryCharge = 60;
+                    refreshCartDetails();
+                }
+                else if(position == 1)
+                {
+                    deliveryCharge = 100;
+                    refreshCartDetails();
+                }
+            }
+        });
     }
 
     private void checkoutOrder()
